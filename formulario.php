ddab5ac9bbb449610bac83ea2726c14ffe30440e
ericklandaverde <harry_erick92@outlook.com>
2015-09-13 02:55:55 -0500

    cambios de frondend

--- formulario.php
+++ formulario.php
@@ -21,7 +21,7 @@ mssql_query("insert empleados(clave_emp, nombre, email, empresa, ciudad, salario
 mssql_close($conexionsql);*/
 
 $sql="insert identificacion(puesto, nombre, direccion, fecha, edad, estadocivil, telefono, nivelacademico) 
-values($puesto','$nombre','$direccion','$fecha','$edad','$estadocivil','$telefono','$nivelacademico')";
+values('$puesto','$nombre','$direccion','$fecha','$edad','$estadocivil','$telefono','$nivelacademico')";
 $registro=mysqli_query($conexion,$sql);
 if(!$registro)
 {
