81dc2613d47b3c2063547d4d22206d67d8e16411
ericklandaverde <harry_erick92@outlook.com>
2015-09-13 16:24:46 -0500

    Modificacion

--- formularioIdentificacion.php
+++ formularioIdentificacion.php
@@ -22,7 +22,7 @@ mssql_query("insert empleados(clave_emp, nombre, email, empresa, ciudad, salario
 mssql_close($conexionsql);*/
 
 $sql="insert identificacion(clave, puesto, nombre, direccion, fecha, edad, estadocivil, telefono, nivelacademico) 
-values('$clave', '$puesto','$nombre','$direccion','$fecha','$edad','$estadocivil','$telefono','$nivelacademico')";
+values('$clave','$puesto','$nombre','$direccion','$fecha','$edad','$estadocivil','$telefono','$nivelacademico')";
 $registro=mysqli_query($conexion,$sql);
 if(!$registro)
 {
