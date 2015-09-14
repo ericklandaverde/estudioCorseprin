c4c06b67413172c2805f3a4c916c655c37382762
ericklandaverde <harry_erick92@outlook.com>
2015-09-13 20:31:40 -0500

    Modificacion

--- reporte.php
+++ reporte.php
@@ -112,7 +112,7 @@ function Header()
 {
     //$this->Image('imagenes/cecyte.jpg' , 20 ,8, 23 , 18,'JPG');
 	//$this->Image('imagenes/letras3.jpg' , 45 ,13, 117 , 20,'JPG');
-	$this->Image('imagenes/logo.png' , 20 ,8, 22 , 18,'PNG');
+	$pdf->Image('img/logo.png' , 10 ,8, 10 , 13,'PNG');
 	//$this->Image('imagenes/images.jpg' , 163 ,10, 35 , 20,'JPG');
 	
 	$this->SetFont('Arial','',11);
