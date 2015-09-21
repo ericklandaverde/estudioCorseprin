b7160eb97e3798ac3116ffefb3c817e0596acd65
ericklandaverde <harry_erick92@outlook.com>
2015-09-21 04:37:40 -0500

    Cambio

--- formularioIdentificacion.php
+++ formularioIdentificacion.php
@@ -13,36 +13,7 @@ $estadocivil=$_POST['estadocivil'];
 $telefono=$_POST['telefono'];
 $email=$_POST['email'];
 $nivelacademico=$_POST['nivelacademico'];
-
- if(isset($_POST['sumit']))
-    {
-        if(getimagesize($_FILES['image']['tmp_name']) == FALSE)
-        {
-            echo"<script language='javascript'>alert('Por favor selecciona una imagen.')</script>";
-        }
-        else
-        {
-            $image= addslashes($_FILES['image']['tmp_name']);
-            $name= addslashes($_FILES['image']['name']);
-            $image= file_get_contents($image);
-            $image= base64_encode($image);
-            saveimage($name,$image);
-        }
-    }
-
-function saveimage($name,$image)
-{
-    $qry="insert into identificacion (name,image) values ('$name','$image')";
-    $result=mysql_query($qry,$con);
-    if($result)
-    {
-        echo"<script language='javascript'>alert('Imagen guardada.')</script>";
-    }
-    else
-    {
-    	echo"<script language='javascript'>alert('Imagen no guardada.')</script>";
-    }
-}
+$image=$_POST['image'];
 
 /*$conexionsql=mssql_connect() or
   die("Error de conexión.");
@@ -52,8 +23,8 @@ mssql_query("insert empleados(clave_emp, nombre, email, empresa, ciudad, salario
   die("Error SQL");
 mssql_close($conexionsql);*/
 
-$sql="insert identificacion(clave, puesto, nombre, direccion, fecha, edad, estadocivil, telefono, email, nivelacademico) 
-values('$clave','$puesto','$nombre','$direccion','$fecha','$edad','$estadocivil','$telefono','email','$nivelacademico')";
+$sql="insert identificacion(clave, puesto, nombre, direccion, fecha, edad, estadocivil, telefono, email, nivelacademico, image) 
+values('$clave','$puesto','$nombre','$direccion','$fecha','$edad','$estadocivil','$telefono','email','$nivelacademico','image')";
 $registro=mysqli_query($conexion,$sql);
 if(!$registro)
 {
