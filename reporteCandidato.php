bee87dd23730e059ec40d5b73e3936cb5df6d1bf
ericklandaverde <harry_erick92@outlook.com>
2015-09-25 02:27:19 -0500

    Cambio en formato pdf primordial

--- reporteCandidato.php
+++ reporteCandidato.php
@@ -4,31 +4,32 @@ require('fpdf/fpdf.php');
 class PDF extends FPDF
 {
 // Cabecera de página
-function Header()
-{
-    // Logo
-    $this->Image('imagenes/logo.png',10,8,33);
-    // Arial bold 15
-    $this->SetFont('Arial','B',15);
-    // Movernos a la derecha
-    $this->Cell(80);
-    // Título
-    $this->Cell(30,10,'Title del documento',1,0,'C');
-    // Salto de línea
-    $this->Ln(20);
+    function Header()
+    {
+        // Logo
+        $this->Image('imagenes/logo.png',0,0,33);
+        // Arial bold 15
+        $this->SetFont('Arial','B',15);
+        // Movernos a la derecha
+        $this->Cell(80);
+        // Título
+        $this->Cell(30,10,'Title del documento',1,0,'C');
+        // Salto de línea
+        $this->Ln(20);
+    }
+
+    // Pie de página
+    function Footer()
+    {
+        // Posición: a 1,5 cm del final
+        $this->SetY(-15);
+        // Arial italic 8
+        $this->SetFont('Arial','I',8);
+        // Número de página
+        $this->Cell(0,10,'Pagina '.$this->PageNo().'/{nb}',0,0,'C');
+    }
 }
 
-// Pie de página
-function Footer()
-{
-    // Posición: a 1,5 cm del final
-    $this->SetY(-15);
-    // Arial italic 8
-    $this->SetFont('Arial','I',8);
-    // Número de página
-    $this->Cell(0,10,'Pagina '.$this->PageNo().'/{nb}',0,0,'C');
-}
-}
 // Creación del objeto de la clase heredada
 $pdf = new PDF();
 $pdf->AliasNbPages();
