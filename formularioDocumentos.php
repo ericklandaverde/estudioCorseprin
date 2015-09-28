8470cecd7f02c85c1484cc2270a4d968efcd00e2
ericklandaverde <harry_erick92@outlook.com>
2015-09-28 18:31:11 -0500

    Cambio

--- formularioDocumentos.php
+++ formularioDocumentos.php
@@ -76,7 +76,7 @@
 					<div class="section-heading">
 					 <h2>ESTUDIO SOCIOECONOMICO</h2>
 					 <p><label>REVISION DE DOCUMENTOS</label></p>
-					 <p>CANDIDATO: <label><?php echo $clave ?></label></p>
+					 <p>CANDIDATO: <label><?php echo $clave; ?></label></p>
 						 <p>Enlace rapido atras <a href="http://estudiocorseprin.pe.hu/formularioIdentificacion.html">Atras </a></p>
 						 <p>Enlace rapido adelante <a href="http://estudiocorseprin.pe.hu/formularioLaboral.html">Adelante </a></p>
 					</div>
@@ -92,7 +92,7 @@
 					<div class="col-md-offset-2 col-md-8">
 					  <label>Actas de registro civil: </label>
 					  <label>Nacimiento:              </label>
-					    <input type="text" class="form-control" id="inputClave" placeholder="Clave" name="clave" required value=<?php echo $clave ?>>
+					    <input type="text" class="form-control" id="inputClave" placeholder="Clave" name="clave" required value="<?php echo $clave; ?>">
 					    <input type="radio" name="nacimiento" value="Si" placeholder="Selecciona una opcion" checked required>Si
 					    <input type="radio" name="nacimiento" value="No" placeholder="Selecciona una opcion" required>No
 					</div>
