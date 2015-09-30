61451f083d9a6441072f3d34872480a680cc7396
ericklandaverde <harry_erick92@outlook.com>
2015-09-29 21:28:21 -0500

    Cambio en formatos

--- formularioIdentificacion.php
+++ formularioIdentificacion.php
@@ -148,46 +148,8 @@
 						<p>La fotografía debe ser un archivo de foto formato tipo: nombre.jpg|nombre.png 
 						<p>De frente, con la frente descubierta, sin barba, sin patilla, sin adornos de ninguna especie, 
 						sin lentes, si usa bigote debe estar recortado y con el cabello corto estilo militar.</p>
-						<?php
-							function LimitAttach(tField,iType) {
-								file=tField.value;
-								if (iType==1) {
-								extArray = new Array(".gif",".jpg",".png");
-								}
-								if (iType==2) {
-								extArray = new Array(".swf");
-								}
-								if (iType==3) {
-								extArray = new Array(".exe",".sit",".zip",".tar",".swf",".mov",".hqx",".ra",".wmf",".mp3",".qt",".med",".et");
-								}
-								if (iType==4) {
-								extArray = new Array(".mov",".ra",".wmf",".mp3",".qt",".med",".et",".wav");
-								}
-								if (iType==5) {
-								extArray = new Array(".html",".htm",".shtml");
-								}
-								if (iType==6) {
-								extArray = new Array(".doc",".xls",".ppt");
-								}
-							    allowSubmit = false;
-							    if (!file) return;
-							     while (file.indexOf("\\") != -1) file = file.slice(file.indexOf("\\") + 1);
-							    ext = file.slice(file.indexOf(".")).toLowerCase();
-							    for (var i = 0; i < extArray.length; i++) {
-								if (extArray[i] == ext) {
-								allowSubmit = true;
-								break;
-								}
-								}
-								if (allowSubmit) {
-								} else {
-								tField.value="";
-								alert("Usted sólo puede subir archivos con extensiones " + (extArray.join(" ")) + "\nPor favor seleccione un nuevo archivo");
-								}
-							}  
-						?>
 
-					  	<input type="file" onBlur="LimitAttach(this,1)" class="form-control" id="inputClave" placeholder="Selecciona una fotografia" name="image" required>
+					  	<input type="file" accept="image/*" class="form-control" id="inputClave" placeholder="Selecciona una fotografia" name="image" required>
 					</div>
 				  </div>
 
