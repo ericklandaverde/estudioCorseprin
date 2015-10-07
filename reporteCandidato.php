019c2b142b4643bea10dd4f6519c0a217793774f
ericklandaverde <harry_erick92@outlook.com>
2015-10-07 05:14:50 -0500

    Cambio en la Imagen

--- reporteCandidato.php
+++ reporteCandidato.php
@@ -180,10 +180,10 @@ class PDF extends FPDF
         $pdf->Cell(30, 8, 'ELEMENTO', 1);$pdf->Cell(30, 8, 'Fecha'.date('d/m/Y').'', 1);
         $pdf->Ln(8);
         $pdf->Cell(30, 8,$fila['nombre'], 1); $pdf->Cell(160, 8,$fila['nombre'], 1);
-        $pdf->Cell(30, 8, 'GERENCIA DE CAPITAL HUMANO', 1); $pdf->Cell(160, 8,$fila['nombre'], 1);
+        $pdf->Cell(30, 8, 'GERENCIA DE CAPITAL HUMANO', 1); $pdf->Cell(30, 8,$fila['nombre'], 1);
         $pdf->Ln(8);
-        $pdf->Cell(30, 8, 'SERVICIO', 1); $pdf->Cell(160, 8,$fila['direccion'], 1);
-        $pdf->Ln(2);
+        $pdf->Cell(30, 8, 'SERVICIO', 1); $pdf->Cell(30, 8,$fila['direccion'], 1);
+        $pdf->Ln(8);
         $pdf->Cell(60,0,"",0,0,'L');$pdf->Image($fila['rutaImagen'],null,null,40,40);
         $pdf->Ln(5);
         $pdf->Cell(0,0,utf8_decode('1.- DATOS DE IDENTIFICACIÓN'),0,0,'L');
