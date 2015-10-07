34cdfb56ce3c40b3a9c98f81d6414c1b987da34b
ericklandaverde <harry_erick92@outlook.com>
2015-10-07 05:58:25 -0500

    Cambio en la Imagen

--- reporteCandidato.php
+++ reporteCandidato.php
@@ -183,7 +183,7 @@ class PDF extends FPDF
         $pdf->Ln(8);
         $pdf->Cell(10,0,"",0,0,'L'); $pdf->Cell(100, 8,"",1,0,'C'); $pdf->Cell(70, 8, 'SERVICIO',1,0,'C');
         $pdf->Ln(9);
-        $pdf->Cell(70,0,"",0,0,'L'); $pdf->Image($fila['rutaImagen'],null,null,50,60);
+        $pdf->Cell(70,0,"",0,0,'L'); $pdf->Image($fila['rutaImagen'],null,null,60,70);
         $pdf->Ln(5);
         $pdf->Cell(10,0,"",0,0,'L'); $pdf->Cell(0,0,utf8_decode('1.- DATOS DE IDENTIFICACIÓN'),0,0,'L');
         $pdf->Ln(2);
@@ -191,12 +191,16 @@ class PDF extends FPDF
         $pdf->Ln(8);
         $pdf->Cell(10,0,"",0,0,'L'); $pdf->Cell(30, 8,'Nombre:',1); $pdf->Cell(140,8,$fila['nombre'],1,0,'C');
         $pdf->Ln(8);
-        $pdf->Cell(10,0,"",0,0,'L'); $pdf->Cell(30, 8,'Direccion:',1); $pdf->Cell(140,8,utf8_decode($fila['direccion']),1);
+        $pdf->Cell(10,0,"",0,0,'L'); $pdf->Cell(30, 8,'Direccion:',1); $pdf->Cell(140,8,utf8_decode($fila['direccion']),);
         $pdf->Ln(8);
-        $pdf->Cell(10,0,"",0,0,'L'); $pdf->Cell(30, 8,'Fecha:',1); $pdf->Cell(30,8,'Edad', 1); $pdf->Cell(30,8,'Estado civil', 1);
+        $pdf->Cell(10,0,"",0,0,'L'); $pdf->Cell(50, 8,'Fecha:',1); $pdf->Cell(50,8,'Edad', 1); $pdf->Cell(50,8,'Estado civil',1,0,'C');
         $pdf->Ln(8);
-        $pdf->Cell(10,0,"",0,0,'L'); $pdf->Cell(30, 8,$fila['fecha'],1,0,'C'); $pdf->Cell(30,8,$fila['edad'],1,0,'C'); $pdf->Cell(30, 8,$fila['estadocivil'],1,0,'C');
-
+        $pdf->Cell(10,0,"",0,0,'L'); $pdf->Cell(50, 8,$fila['fecha'],1,0,'C'); $pdf->Cell(50,8,$fila['edad'],1,0,'C'); $pdf->Cell(50, 8,$fila['estadocivil'],1,0,'C');
+        $pdf->Ln(8);
+        $pdf->Cell(10,0,"",0,0,'L'); $pdf->Cell(100, 8,'Telefono:',1); $pdf->Cell(60,8,'Nivel Academico',1,0,'C');
+        $pdf->Ln(8);
+        $pdf->Cell(10,0,"",0,0,'L'); $pdf->Cell(100, 8,$fila['telefono'],1,0,'C'); $pdf->Cell(60, 8,$fila['nivelacademico'],1,0,'C');
+ 
         //Conteo de paginas
         $pdf->AliasNbPages();
          //Final de pdf
