ff11dc0a9daa1e24a4cce3286220f50db3042453
ericklandaverde <harry_erick92@outlook.com>
2015-10-12 08:15:33 -0500

    Cambios formulario economico

--- formularioInvSocialFamiliar.php
+++ formularioInvSocialFamiliar.php
@@ -36,10 +36,8 @@
 	$montoOchoE=$_POST['montoOchoE'];
 	$totalEgresos=$_POST['totalEgresos'];
 
-	$sqlE="insert economicoEgresos(id_rfc, personaUnoE, montoUnoE, personaDosE, montoDosE, personaTresE, montoTresE, personaCuatroE, montoCuatroE, personaCincoE,
-		montoCincoE, personaSeisE, montoSeisE, personaSieteE montoSieteE, personaOchoE, montoOchoE, totalEgresos) 
-	values('$clave','$personaUnoE','$montoUnoE','$personaDosE','$montoDosE','$personaTresE','$montoTresE','$personaCuatroE','$montoCuatroE','$personaCincoE',
-		'$montoCincoE','$personaSeisE','$montoSeisE','$personaSieteE','$montoSieteE','$personaOchoE','$montoOchoE','$totalEgresos')";
+	$sqlE="insert economicoEgresos(id_rfc, personaUnoE, montoUnoE, personaDosE, montoDosE, personaTresE, montoTresE, personaCuatroE, montoCuatroE, personaCincoE, montoCincoE, personaSeisE, montoSeisE, personaSieteE montoSieteE, personaOchoE, montoOchoE, totalEgresos) 
+	values('$clave','$personaUnoE','$montoUnoE','$personaDosE','$montoDosE','$personaTresE','$montoTresE','$personaCuatroE','$montoCuatroE','$personaCincoE','$montoCincoE','$personaSeisE','$montoSeisE','$personaSieteE','$montoSieteE','$personaOchoE','$montoOchoE','$totalEgresos')";
 	$registroE=mysqli_query($conexion,$sqlE);
 
 	//economicoResumen
