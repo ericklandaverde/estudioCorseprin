213af88fc4653c7a073482608249bbad1bbab609
ericklandaverde <harry_erick92@outlook.com>
2015-10-13 01:11:23 -0500

    Cambios formulario economico

--- formularioFinal.php
+++ formularioFinal.php
@@ -9,21 +9,21 @@
 	$edad=$_POST['edad'];
 	$ocupacion=$_POST['ocupacion'];
 	$depende=$_POST['depende'];
-	$parentesco=$_POST['parentescoDos'];
-	$nombre=$_POST['nombreDos'];
-	$edad=$_POST['edadDos'];
-	$ocupacion=$_POST['ocupacionDos'];
-	$depende=$_POST['dependeDos'];
-	$parentesco=$_POST['parentescoTres'];
-	$nombre=$_POST['nombreTres'];
-	$edad=$_POST['edadTres'];
-	$ocupacion=$_POST['ocupacionTres'];
-	$depende=$_POST['dependeTres'];
-	$parentesco=$_POST['parentescoCuatro'];
-	$nombre=$_POST['nombreCuatro'];
-	$edad=$_POST['edadCuatro'];
-	$ocupacion=$_POST['ocupacionCuatro'];
-	$depende=$_POST['dependeCuatro'];
+	$parentescoDos=$_POST['parentescoDos'];
+	$nombreDos=$_POST['nombreDos'];
+	$edadDos=$_POST['edadDos'];
+	$ocupacionDos=$_POST['ocupacionDos'];
+	$dependeDos=$_POST['dependeDos'];
+	$parentescoTres=$_POST['parentescoTres'];
+	$nombreTres=$_POST['nombreTres'];
+	$edadTres=$_POST['edadTres'];
+	$ocupacionTres=$_POST['ocupacionTres'];
+	$dependeTres=$_POST['dependeTres'];
+	$parentescoCuatro=$_POST['parentescoCuatro'];
+	$nombreCuatro=$_POST['nombreCuatro'];
+	$edadCuatro=$_POST['edadCuatro'];
+	$ocupacionCuatro=$_POST['ocupacionCuatro'];
+	$dependeCuatro=$_POST['dependeCuatro'];
 	
 	$sqlDatos="insert familiarDatos(id_rfc, parentesco, nombre, edad, ocupacion, depende, parentescoDos, nombreDos, edadDos, ocupacionDos, dependeDos, parentescoTres, nombreTres, edadTres, ocupacionTres, dependeTres, parentescoCuatro, nombreCuatro, edadCuatro, ocupacionCuatro, dependeCuatro) 
 	values('$clave','$parentesco','$nombre','$edad','$ocupacion','$depende','$parentescoDos','$nombreDos','$edadDos','$ocupacionDos','$dependeDos','$parentescoTres','$nombreTres','$edadTres','$ocupacionTres','$dependeTres','$parentescoCuatro','$nombreCuatro','$edadCuatro','$ocupacionCuatro','$dependeCuatro')";
