30161950582d1aa73fc559cf18cf012a09077d6e
ericklandaverde <harry_erick92@outlook.com>
2015-10-14 13:02:11 -0500

    Modificaciones en formularios

--- formularioInvSocialFamiliar.php
+++ formularioInvSocialFamiliar.php
@@ -122,6 +122,35 @@ else
 	<link href="css/style.css" rel="stylesheet" media="screen">
 	<link href="color/default.css" rel="stylesheet" media="screen">
 	<script src="js/modernizr.custom.js"></script>
+	<script type="text/javascript">
+			$(document).ready(function() {
+		    $('#btnDel').attr('disabled','disabled');
+		    $('#btnAdd').click(function() {
+		      var num = $('.clonedInput').length; // how many "duplicatable" input fields we currently have
+		      var newNum = new Number(num + 1); // the numeric ID of the new input field being added
+		      // create the new element via clone(), and manipulate it's ID using newNum value
+		      var newElem = $('#input' + num).clone().attr('id', 'Add' + newNum);
+		      // manipulate the name/id values of the input inside the new element
+		      newElem.children(':last').attr('id', 'name' + newNum).attr('name', 'name' + newNum);
+		      // insert the new element after the last "duplicatable" input field
+		      $('#input' + num).after(newElem);
+		      // enable the "remove" button
+		      $('#btnDel').attr('disabled',false);
+		      // business rule: you can only add 10 names
+		      if (newNum == 10)
+		        $('#btnAdd').attr('disabled','disabled');
+		    });
+		    $('#btnDel').click(function() {
+		      var num = $('.clonedInput').length; // how many "duplicatable" input fields we currently have
+		      $('#input' + num).remove(); // remove the last element
+		      // enable the "add" button
+		      $('#btnAdd').attr('disabled',false);
+		      // if only one element remains, disable the "remove" button
+		      if (num-1 == 1)
+		        $('#btnDel').attr('disabled','disabled');
+		    });
+		  });
+	</script>
 </head>
 
 <body>
@@ -441,6 +470,17 @@ else
 					</form>
 				</div>
 			</div>
+		    <form id="testform">
+			   <fieldset id="input1" class="clonedInput">
+			      <label>Nombre</label>
+			      <input type="text" name="name1" id="name1" />
+			   </fieldset>
+			   <fieldset>
+			      <label>Agregar otro familiar?</label>
+			      <inputtype="button" id="btnAdd" value="+" />
+			      <input type="button" id="btnDel" value="-" />
+			   </fieldset>
+			</form>
 			<div class="row mar-top30 ">
 				<div class="col-md-offset-2 col-md-8">
 					<h5>Tienes dudas contactanos por nuestras redes sociales.</h5>
