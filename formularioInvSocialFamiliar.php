6dfd013c5ab63ca75fedfbd8efcf838047432ada
ericklandaverde <harry_erick92@outlook.com>
2015-10-14 18:44:54 -0500

    Cambios formulario economico

--- formularioInvSocialFamiliar.php
+++ formularioInvSocialFamiliar.php
@@ -127,42 +127,42 @@ else
 	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.3/jquery.min.js"></script>
 	<script src="js/modernizr.custom.js"></script>
     <script type="text/javascript">
-        $(document).ready(function() {
-            $('#btnAdd').click(function() {
-                var num  = $('.clonedInput').length; // how many "duplicatable" input fields we currently have
-                var newNum  = new Number(num + 1);      // the numeric ID of the new input field being added
- 
-                // create the new element via clone(), and manipulate it's ID using newNum value
-                var newElem = $('#input' + num).clone().attr('id', 'input' + newNum);
- 
-                // manipulate the name/id values of the input inside the new element
-                newElem.children(':first').attr('id', 'name' + newNum).attr('name', 'name' + newNum);
- 
-                // insert the new element after the last "duplicatable" input field
-                $('#input' + num).after(newElem);
- 
-                // enable the "remove" button
-                $('#btnDel').attr('disabled','');
- 
-                // business rule: you can only add 5 names
-                if (newNum == 5)
-                    $('#btnAdd').attr('disabled','disabled');
-            });
- 
-            $('#btnDel').click(function() {
-                var num = $('.clonedInput').length; // how many "duplicatable" input fields we currently have
-                $('#input' + num).remove();     // remove the last element
- 
-                // enable the "add" button
-                $('#btnAdd').attr('disabled','');
- 
-                // if only one element remains, disable the "remove" button
-                if (num-1 == 1)
-                    $('#btnDel').attr('disabled','disabled');
-            });
- 
-            $('#btnDel').attr('disabled','disabled');
-        });
+$(document).ready(function() {
+  $('#btnDel').attr('disabled','disabled');
+  $('#btnAdd').click(function() {
+    var num = $('.clonedInput').length; // how many "duplicatable" input fields we currently have
+    var newNum = new Number(num + 1); // the numeric ID of the new input field being added
+
+    // create the new element via clone(), and manipulate it's ID using newNum value
+    var newElem = $('#input' + num).clone().attr('id', 'Add' + newNum);
+
+    // manipulate the name/id values of the input inside the new element
+    newElem.children(':last').attr('id', 'name' + newNum).attr('name', 'name' + newNum);
+
+    // insert the new element after the last "duplicatable" input field
+    $('#input' + num).after(newElem);
+
+    // enable the "remove" button
+    $('#btnDel').attr('disabled',false);
+
+    // business rule: you can only add 10 names
+    if (newNum == 10)
+      $('#btnAdd').attr('disabled','disabled');
+  });
+
+  $('#btnDel').click(function() {
+    var num = $('.clonedInput').length; // how many "duplicatable" input fields we currently have
+    $('#input' + num).remove(); // remove the last element
+
+    // enable the "add" button
+    $('#btnAdd').attr('disabled',false);
+
+    // if only one element remains, disable the "remove" button
+    if (num-1 == 1)
+      $('#btnDel').attr('disabled','disabled');
+  });
+
+});
     </script>
 </head>
 
@@ -483,16 +483,17 @@ else
 					</form>
 				</div>
 			</div>
-<form id="myForm">
-    <div id="input1" style="margin-bottom:4px;" class="clonedInput">
-        Name: <input type="text" name="name1" id="name1" />
-    </div>
- 
-    <div>
-        <input type="button" id="btnAdd" value="add another name" />
-        <input type="button" id="btnDel" value="remove name" />
-    </div>
-</form>   
+<form id="testform">
+   <fieldset id="input1" class="clonedInput">
+      <label>Name</label>
+      <input type="text" name="name1" id="name1" />
+   </fieldset>
+   <fieldset>
+      <label>Need more fields?</label>
+      <inputtype="button" id="btnAdd" value="+" />
+      <input type="button" id="btnDel" value="-" />
+   </fieldset>
+</form> 
 			<div class="row mar-top30 ">
 				<div class="col-md-offset-2 col-md-8">
 					<h5>Tienes dudas contactanos por nuestras redes sociales.</h5>
@@ -524,7 +525,6 @@ else
 					</ul>
 				</div>				
 			</div>
-
 		</div>
 	</section>  
 
