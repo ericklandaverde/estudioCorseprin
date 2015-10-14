ed8768492a4aac335d4fab04b39846c12bce6d22
ericklandaverde <harry_erick92@outlook.com>
2015-10-14 18:28:20 -0500

    Cambios formulario economico

--- formularioInvSocialFamiliar.php
+++ formularioInvSocialFamiliar.php
@@ -129,25 +129,34 @@ else
     <script type="text/javascript">
         $(document).ready(function() {
             $('#btnAdd').click(function() {
-                var num     = $('.clonedInput').length;
-                var newNum  = new Number(num + 1);
+                var num     = $('.clonedInput').length; // how many "duplicatable" input fields we currently have
+                var newNum  = new Number(num + 1);      // the numeric ID of the new input field being added
  
+                // create the new element via clone(), and manipulate it's ID using newNum value
                 var newElem = $('#input' + num).clone().attr('id', 'input' + newNum);
-     
+ 
+                // manipulate the name/id values of the input inside the new element
                 newElem.children(':first').attr('id', 'name' + newNum).attr('name', 'name' + newNum);
+ 
+                // insert the new element after the last "duplicatable" input field
                 $('#input' + num).after(newElem);
+ 
+                // enable the "remove" button
                 $('#btnDel').attr('disabled','');
  
+                // business rule: you can only add 5 names
                 if (newNum == 5)
                     $('#btnAdd').attr('disabled','disabled');
             });
  
             $('#btnDel').click(function() {
-                var num = $('.clonedInput').length;
+                var num = $('.clonedInput').length; // how many "duplicatable" input fields we currently have
+                $('#input' + num).remove();     // remove the last element
  
-                $('#input' + num).remove();
+                // enable the "add" button
                 $('#btnAdd').attr('disabled','');
  
+                // if only one element remains, disable the "remove" button
                 if (num-1 == 1)
                     $('#btnDel').attr('disabled','disabled');
             });
@@ -224,7 +233,6 @@ else
 									</tr>
 								</table>
 								</fieldset>
-								<fieldset>
 <form id="myForm">
     <div id="input1" style="margin-bottom:4px;" class="clonedInput">
         Name: <input type="text" name="name1" id="name1" />
@@ -234,7 +242,9 @@ else
         <input type="button" id="btnAdd" value="add another name" />
         <input type="button" id="btnDel" value="remove name" />
     </div>
-</form>                                <legend>Familiar Dos</legend>
+</form>                               
+                            <fieldset>
+                                <legend>Familiar Dos</legend>
 								<table>
 									<tr>
 										<td><label>Parentesco</label></td>
