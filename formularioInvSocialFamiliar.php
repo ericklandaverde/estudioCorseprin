ae67cd591995020c6655b0c265e12f618b5d4e3a
ericklandaverde <harry_erick92@outlook.com>
2015-10-15 00:26:32 -0500

    Cambios formulario economico

--- formularioInvSocialFamiliar.php
+++ formularioInvSocialFamiliar.php
@@ -80,6 +80,7 @@
 				    <div id="input1" class="clonedInput">
 				    	<fieldset>
                            <legend>Familiar</legend>   
+			    	       <table>
 			    	       <select type="text" class="form-control" id="parentesco" placeholder="Parentesco" name="parentesco" required>
                                <option selected value="">Seleccione un parentesco</option>
                                <option value="Hijo(a)">Hijo(a)</option>
@@ -90,8 +91,9 @@
 			               <input type="text" class="form-control" id="name1" placeholder="Nombre" name="name1" required>
 			               <input type="number" class="form-control" id="edad" placeholder="Edad" name="edad" required>
 			               <input type="text" class="form-control" id="ocupacion" placeholder="Ocupacion" name="ocupacion" required>
-			               <input type="radio" class="form-control" id="depende" name="depende" value="Si" placeholder="Selecciona una opcion" checked required>Si
-                           <input type="radio" class="form-control" id="depende" name="depende" value="No" placeholder="Selecciona una opcion" required>No
+			               <input type="radio" id="depende" name="depende" value="Si" placeholder="Selecciona una opcion" checked required>Si
+                           <input type="radio" id="depende" name="depende" value="No" placeholder="Selecciona una opcion" required>No
+			    	       </table>
                          </fieldset>
 				    </div>
 				 
