a901e22428fda09fbe0cb3e959587a05c1498711
ericklandaverde <harry_erick92@outlook.com>
2015-10-15 14:13:04 -0500

    Cambios en formulario economico

--- formularioInvSocialFamiliar.php
+++ formularioInvSocialFamiliar.php
@@ -190,66 +190,28 @@ else
 			<form id="formulario" action="formularioFinal.php" method="post" class="form-horizontal" role="form">
 				    <label> a) Datos Familiares (Personas con las que vive) </label>
 				    
-<!--         		    <div class="form-group">
-        		    	<div class="col-md-offset-2 col-md-8">
-        				    <div id="input1" class="clonedInput">
-        				    	<fieldset>
-                                   <legend>Familiar</legend>   
-        			    	       <table>
-        			    	       	<tr>
-                                        <td><label>Parentesco</label></td>
-                                        <td><label>Nombre:</label></td>
-                                    </tr>
-                                    <tr>
-                                    <td>                           
-                                        <select type="text" class="form-control" id="parentesco1" placeholder="Parentesco" name="parentesco1" required>
-                                           <option selected value="">Seleccione un parentesco</option>
-                                           <option value="Hijo(a)">Hijo(a)</option>
-                                           <option value="Esposo(a)">Esposa(a)</option>
-                                           <option value="Padres">Padres</option>
-                                           <option value="Hermanos">Padres</option>
-                                       </select>
-                                    </td>
-                                    	<td><input type="text" class="form-control" id="name1" placeholder="Nombre" name="name1" required></td>
-                                    </tr>
-                                    <tr>
-                                        <td><label>Edad</label></td>
-                                        <td><label>Ocupacion:</label></td>
-                                        <td><label>Depende economicamente:</label></td>
-                                    </tr>
-                                    <tr>
-                                        <td><input type="text" class="form-control" id="edad1" placeholder="Edad" name="edad1" required></td>
-                                        <td><input type="text" class="form-control" id="ocupacion1" placeholder="Ocupacion" name="ocupacion1" required></td>
-                                        <td>
-                                            <input type="radio" id="depende1" name="depende1" value="Si" placeholder="Selecciona una opcion" checked required>Si
-                                            <input type="radio" id="depende1" name="depende1" value="No" placeholder="Selecciona una opcion" required>No
-                                        </td>
-                                    </tr>
-                                   </table>
-                                 </fieldset>
-        				    </div>
-        				</div>
-        			</div> -->
                     <div class="form-group" id="container">
-                        <h3>Familiares</h3>
-                    <div class="col-md-offset-2 col-md-8" id="myForm">
-                          <div id="cosponsors" style="padding:12px;">
-                            <label>Familiar:</label>
-                            <select type="text" class="form-control" id="parentesco" placeholder="Parentesco" name="parentesco" title="Campo parentesco" required>
-                                                <option selected value="">Seleccione un parentesco</option>
-                                                <option value="Hijo(a)">Hijo(a)</option>
-                                                <option value="Esposo(a)">Esposa(a)</option>
-                                                <option value="Padres">Padres</option>
-                                                <option value="Hermanos">Padres</option>
-                            </select>
-                            <input  type="text" class="form-control" id="nombre" name="nombre"  placeholder="Nombre" title="Campo nombre" required/>
-                            <input  type="text" class="form-control" id="edad"name="edad" placeholder="Edad" title="Campo edad" required/>
-                            <input  type="text" class="form-control" id="ocupacion" name="ocupacion" placeholder="Ocupacion" title="Campo ocupacion" required/>
-                            <input type="radio" name="depende" value="Si" placeholder="Selecciona una opcion" checked required>Si
-                            <input type="radio" name="depende" value="No" placeholder="Selecciona una opcion" required>No
-                          </div>
-                    </div>
-                            <input type="button" class="addRow btn btn-primary" value="Agregar familiar" />
+                        <h5>Familiares: </h5>
+                        <div class="col-md-offset-2 col-md-8" id="myForm">
+                                  <div id="cosponsors" style="padding:12px;">
+                                    <label>Familiar:</label>
+                                    <select type="text" class="form-control" id="parentesco" placeholder="Parentesco" name="parentesco" title="Campo parentesco" required>
+                                        <option selected value="">Seleccione un parentesco</option>
+                                        <option value="Hijo(a)">Hijo(a)</option>
+                                        <option value="Esposo(a)">Esposa(a)</option>
+                                        <option value="Padres">Padres</option>
+                                        <option value="Hermanos">Padres</option>
+                                    </select>
+                                    <input  type="text" class="form-control" id="nombre" name="nombre"  placeholder="Nombre" title="Campo nombre" required/>
+                                    <input  type="number" class="form-control" id="edad"name="edad" placeholder="Edad" title="Campo edad" required/>
+                                    <input  type="text" class="form-control" id="ocupacion" name="ocupacion" placeholder="Ocupacion" title="Campo ocupacion" required/>
+                                    <input type="radio" name="depende" value="Si" placeholder="Selecciona una opcion" checked required>Si
+                                    <input type="radio" name="depende" value="No" placeholder="Selecciona una opcion" required>No
+                                  </div>
+                            </div>
+                                    <input type="button" class="addRow btn btn-primary" value="Agregar familiar" />
+                            </div>
+                        </div>
                     </div>
                     
                     <div class="form-group">
