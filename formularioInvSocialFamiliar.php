8276594b5760678fda0b135d6d12df53346358e7
ericklandaverde <harry_erick92@outlook.com>
2015-10-15 12:48:19 -0500

    Cambios en formulario economico

--- formularioInvSocialFamiliar.php
+++ formularioInvSocialFamiliar.php
@@ -234,17 +234,17 @@ else
                     <div id="myForm">
                           <div id="cosponsors" style="padding:12px;">
                             <label>Familiar:</label>
-                            <select type="text" id="parentesco" placeholder="Parentesco" name="parentesco" title="Campo parentesco" required>
+                            <select type="text" class="form-control" id="parentesco" placeholder="Parentesco" name="parentesco" title="Campo parentesco" required>
                                                 <option selected value="">Seleccione un parentesco</option>
                                                 <option value="Hijo(a)">Hijo(a)</option>
                                                 <option value="Esposo(a)">Esposa(a)</option>
                                                 <option value="Padres">Padres</option>
                                                 <option value="Hermanos">Padres</option>
                             </select>
-                            <input  type="text" id="nombre" name="nombre"  placeholder="Nombre" title="Campo nombre" required/>
-                            <input  type="text" id="edad"name="edad" placeholder="Edad" title="Campo edad" required/>
-                            <input  type="text" id="ocupacion" name="ocupacion" placeholder="Ocupacion" title="Campo ocupacion" required/>
-                            <input  type="text" id="depende" name="depende" placeholder="Depende" title="Campo depende" required/>
+                            <input  type="text" class="form-control" id="nombre" name="nombre"  placeholder="Nombre" title="Campo nombre" required/>
+                            <input  type="text" class="form-control" id="edad"name="edad" placeholder="Edad" title="Campo edad" required/>
+                            <input  type="text" class="form-control" id="ocupacion" name="ocupacion" placeholder="Ocupacion" title="Campo ocupacion" required/>
+                            <input  type="text" class="form-control" id="depende" name="depende" placeholder="Depende" title="Campo depende" required/>
                           </div>
                     </form>
                             <input type="button" class="addRow btn btn-primary" value="Agregar familiar" />
