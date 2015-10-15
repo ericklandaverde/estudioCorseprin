00b4724d049df7259a11869c44e6a975062cd58a
ericklandaverde <harry_erick92@outlook.com>
2015-10-14 23:05:28 -0500

    Cambios formulario economico

--- formularioInvSocialFamiliar.php
+++ formularioInvSocialFamiliar.php
@@ -127,8 +127,8 @@
 	</footer> -->
 
 	<!-- js -->
-    <script src="js/jquery.js"></script>
-<!-- 	// <script src="js/bootstrap.min.js"></script>  -->
+    <!-- <script src="js/jquery.js"></script> -->
+	<script src="js/bootstrap.min.js"></script> 
 	<script src="js/jquery.smooth-scroll.min.js"></script>
 	<script src="js/jquery.dlmenu.js"></script>
 	<script src="js/wow.min.js"></script>
