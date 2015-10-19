d66cac55685b525e2ee4179d12fbc73418651bf6
ericklandaverde <harry_erick92@outlook.com>
2015-10-19 13:47:16 -0500

    Cambios en formulario economico

--- formularioEconomico.php
+++ formularioEconomico.php
@@ -12,13 +12,13 @@ $direccion=$_POST['direccion'];
 $telefono=$_POST['telefono'];
 $comentarios=$_POST['comentarios'];
 
-$nombreDos=$_POST['nombreDos'];
-$ocupacionDos=$_POST['ocupacionDos'];
-$tipoDos=$_POST['tipoDos'];
-$tiempoDos=$_POST['tiempoDos'];
-$direccionDos=$_POST['direccionDos'];
-$telefonoDos=$_POST['telefonoDos'];
-$comentariosDos=$_POST['comentariosDos'];
+$nombreDos=$_POST['nombre_2'];
+$ocupacionDos=$_POST['ocupacion_2'];
+$tipoDos=$_POST['tipo_2'];
+$tiempoDos=$_POST['tiempo_2'];
+$direccionDos=$_POST['direccion_2'];
+$telefonoDos=$_POST['telefono_2'];
+$comentariosDos=$_POST['comentarios_2'];
 
 $sql="insert referencias(id_rfc, nombre, ocupacion, tipo, tiempo, direccion, telefono, comentarios, nombreDos, ocupacionDos, tipoDos, tiempoDos, direccionDos, telefonoDos, comentariosDos) 
 values('$clave','$nombre','$ocupacion','$tipo','$tiempo','$direccion','$telefono','$comentarios','$nombreDos','$ocupacionDos','$tipoDos','$tiempoDos','$direccionDos','$telefonoDos','$comentariosDos')";
