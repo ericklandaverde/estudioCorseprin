e95adb1c2ffa10dace2d9e62e14fc83976725b8e
ericklandaverde <harry_erick92@outlook.com>
2015-10-19 10:34:16 -0500

    Cambios en formulario economico

--- eliminarCandidato.php
+++ eliminarCandidato.php
@@ -16,18 +16,23 @@ $registrosL = mysqli_query($conexion,$sqlL);
 $sqlR="DELETE FROM referencias WHERE id_rfc='$clave'";
 $registrosR = mysqli_query($conexion,$sqlR);
 
-$sqlIng="DELETE FROM economicoIngresos WHERE id_rfc='$clave'";
-$registrosIng = mysqli_query($conexion,$sqlIng);
-$sqlEgr="DELETE FROM economicoEgresos WHERE id_rfc='$clave'";
-$registrosEgr = mysqli_query($conexion,$sqlEgr);
-$sqlRes="DELETE FROM economicoResumen WHERE id_rfc='$clave'";
-$registrosRes = mysqli_query($conexion,$sqlRes);
-$sqlCre="DELETE FROM economicoCreditos WHERE id_rfc='$clave'";
-$registrosCre = mysqli_query($conexion,$sqlCre);
-$sqlSeg="DELETE FROM economicoSeguro WHERE id_rfc='$clave'";
-$registrosSeg = mysqli_query($conexion,$sqlSeg);
-$sqlAct="DELETE FROM economicoActivos WHERE id_rfc='$clave'";
-$registrosAct = mysqli_query($conexion,$sqlAct);
+	$sqlIng="DELETE FROM economicoIngresos WHERE id_rfc='$clave'";
+	$registrosIng = mysqli_query($conexion,$sqlIng);
+	$sqlEgr="DELETE FROM economicoEgresos WHERE id_rfc='$clave'";
+	$registrosEgr = mysqli_query($conexion,$sqlEgr);
+	$sqlRes="DELETE FROM economicoResumen WHERE id_rfc='$clave'";
+	$registrosRes = mysqli_query($conexion,$sqlRes);
+	$sqlCre="DELETE FROM economicoCreditos WHERE id_rfc='$clave'";
+	$registrosCre = mysqli_query($conexion,$sqlCre);
+	$sqlSeg="DELETE FROM economicoSeguro WHERE id_rfc='$clave'";
+	$registrosSeg = mysqli_query($conexion,$sqlSeg);
+	$sqlAct="DELETE FROM economicoActivos WHERE id_rfc='$clave'";
+	$registrosAct = mysqli_query($conexion,$sqlAct);
+
+$sqlDatos="DELETE FROM familiarDatos WHERE id_rfc='$clave'";
+$registrosDatos = mysqli_query($conexion,$sqlDatos);
+$sqlActividades="DELETE FROM familiarActividades WHERE id_rfc='$clave'";
+$registrosActividades = mysqli_query($conexion,$sqlActividades);
 
 // $sql="DELETE FROM identificacion WHERE id_rfc='$clave'";
 // $registros = mysqli_query($conexion,$sql);
