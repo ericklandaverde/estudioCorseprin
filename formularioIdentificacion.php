c5d1c5c51100c113657701bc23f46406491f7a1d
ericklandaverde <harry_erick92@outlook.com>
2015-10-19 11:07:51 -0500

    Cambios en formulario economico

--- formularioIdentificacion.php
+++ formularioIdentificacion.php
@@ -27,7 +27,7 @@
 	    $('#insertar').click(function() {
 	        if ($('#telefono').val().length != 8 || isNaN($('#telefono').val())) {
 	            $('#telefono').css('border-color','#FF0000');
-	            alert('El número de teléfono debe tener 8 números.');
+	            alert('El número de teléfono solo 8 números.');
 	            return false;
 	        }
 	        else {
