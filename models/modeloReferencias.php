623570e74954bea52bcdd1d698d3bc02a12ab307
ericklandaverde <harry_erick92@outlook.com>
2015-10-20 21:32:42 -0500

    Cambios en formulario economico

--- models/modeloReferencias.php
+++ models/modeloReferencias.php
@@ -14,28 +14,28 @@ $direccion=$_POST['direccion'];
 $telefono=$_POST['telefono'];
 $comentario=$_POST['comentario'];
 
-$nombre2=$_POST['nombre1'];
-$ocupacion2=$_POST['ocupacion1'];
-$tipo2=$_POST['tipo1'];
-$tiempo2=$_POST['tiempo1'];
-$direccion2=$_POST['direccion1'];
-$telefono2=$_POST['telefono1'];
-$comentario2=$_POST['comentario1'];
+$nombre1=$_POST['nombre1'];
+$ocupacion1=$_POST['ocupacion1'];
+$tipo1=$_POST['tipo1'];
+$tiempo1=$_POST['tiempo1'];
+$direccion1=$_POST['direccion1'];
+$telefono1=$_POST['telefono1'];
+$comentario1=$_POST['comentario1'];
 
-$nombre3=$_POST['nombre2'];
-$ocupacion3=$_POST['ocupacion2'];
-$tipo3=$_POST['tipo2'];
-$tiempo3=$_POST['tiempo2'];
-$direccion3=$_POST['direccion2'];
-$telefono3=$_POST['telefono2'];
-$comentario3=$_POST['comentario2'];
+$nombre2=$_POST['nombre2'];
+$ocupacion2=$_POST['ocupacion2'];
+$tipo2=$_POST['tipo2'];
+$tiempo2=$_POST['tiempo2'];
+$direccion2=$_POST['direccion2'];
+$telefono2=$_POST['telefono2'];
+$comentario2=$_POST['comentario2'];
 
 $sql="insert referencias(id_rfc, nombre, ocupacion, tipo, tiempo, direccion, telefono, comentario, 
-    nombre1, ocupacion1, tipo2, tiempo1, direccion1, telefono1, comentario1, 
-    nombre2, ocupacion2, tipo3, tiempo2, direccion2, telefono2, comentario2) 
+    nombre1, ocupacion1, tipo1, tiempo1, direccion1, telefono1, comentario1, 
+    nombre2, ocupacion2, tipo2, tiempo2, direccion2, telefono2, comentario2) 
 values('$clave','$nombre','$ocupacion','$tipo','$tiempo','$direccion','$telefono','$comentario',
-    '$nombre1','$ocupacion1','$tipo2','$tiempo1','$direccion1','$telefono1','$comentario1',
-    '$nombre2','$ocupacion2','$tipo3','$tiempo2','$direccion2','$telefono2','$comentario2')";
+    '$nombre1','$ocupacion1','$tipo1','$tiempo1','$direccion1','$telefono1','$comentario1',
+    '$nombre2','$ocupacion2','$tipo2','$tiempo2','$direccion2','$telefono2','$comentario2')";
 
 $registro=mysqli_query($conexion,$sql);
 if(!$registro)
