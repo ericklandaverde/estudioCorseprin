113b16dba26926ed0edbe5ad3a2d110e49cd3053
ericklandaverde <harry_erick92@outlook.com>
2015-12-04 17:37:17 -0600

    Cambios

--- Login-ajax/db.php
+++ Login-ajax/db.php
@@ -1,5 +1,5 @@
 <?php
-$conexion = mysqli_connect('mysql.hostinger.es','u802532598_usuar','kDu14GQj2G','u802532598_login') or die ('Error de Conexion de Servidor: ' .  mysqli_error());
-$db=mysqli_select_db($conexion, 'u802532598_login') or die ('Error al seleccionar la Base de Datos: '.mysql_error());
+$conexion = mysqli_connect('mysql.hostinger.es','u375487675_usuar','ittla6021','u375487675_login') or die ('Error de Conexion de Servidor: ' .  mysqli_error());
+$db=mysqli_select_db($conexion, 'u375487675_login') or die ('Error al seleccionar la Base de Datos: '.mysql_error());
 mysqli_query ($conexion,"SET NAMES 'utf8'");
 ?>
