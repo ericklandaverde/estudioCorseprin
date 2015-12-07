3dfa0a82d5058db124414c3ce57469347fb1fe44
ericklandaverde <harry_erick92@outlook.com>
2015-12-07 10:46:55 -0600

    Cambios formulario documentos

--- formularioDocumentos.php
+++ formularioDocumentos.php
@@ -70,8 +70,8 @@ session_start();
 					  <label>Identificacion peronal </label><br>
 					  <div class="alert alert-info">
 					  	<label>Cuenta con Credencial de elector:</label>
-					  	    Si: <input type="radio" name="myRadioButton" onclick="elector.disabled=false"/>
-							No: <input type="radio" name="myRadioButton" onclick="elector.disabled=true" checked/>
+					  	    Si: <input type="radio" name="elector" onclick="elector.disabled=false"/>
+							No: <input type="radio" name="elector" onclick="elector.disabled=true" checked/>
 					  </div>
 					  <div class="input-group">
 							<span class="input-group-addon"><label>Credencial  de elector: </label></span>
@@ -79,8 +79,8 @@ session_start();
 					  </div><br>
                       <div class="alert alert-info">
                       	<label>Cuenta con Cartilla Militar</label>
-					  	    Si: <input type="radio" name="myRadioButton" onclick="militar.disabled=false"/>
-							No: <input type="radio" name="myRadioButton" onclick="militar.disabled=true" checked/>
+					  	    Si: <input type="radio" name="militar" onclick="militar.disabled=false"/>
+							No: <input type="radio" name="militar" onclick="militar.disabled=true" checked/>
                       </div>
 					  <div class="input-group">
 							<span class="input-group-addon"><label>Cartilla Militar: </label></span>
@@ -88,8 +88,8 @@ session_start();
 					  </div><br>
                       <div class="alert alert-info">
                       	<label>Cuenta con Licencia de conducir</label>
-					  	    Si: <input type="radio" name="myRadioButton" onclick="licencia.disabled=false"/>
-							No: <input type="radio" name="myRadioButton" onclick="licencia.disabled=true" checked/>
+					  	    Si: <input type="radio" name="conducir" onclick="licencia.disabled=false"/>
+							No: <input type="radio" name="conducir" onclick="licencia.disabled=true" checked/>
                       </div>
 					  <div class="input-group">
 							<span class="input-group-addon"><label>Licencia de conducir: </label></span>
